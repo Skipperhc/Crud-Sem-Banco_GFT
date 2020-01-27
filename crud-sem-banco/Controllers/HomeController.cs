f7f08afb1c891d6ab41fdf604d5628bdbe5eee1c
skipperhc <vitorhainosz2@gmail.com>
2020-01-27 15:23:32 -0300

    rever depois

---
+++ crud-sem-banco/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using crud_sem_banco.Models;
+
+namespace crud_sem_banco.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+
+            return RedirectToAction("Index", "Pessoa");
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ crud-sem-banco/Controllers/PessoaController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crud_sem_banco.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace crud_sem_banco.Controllers {
+    public class PessoaController : Controller {
+        private readonly List<Pessoa> _listaPessoas;
+        public PessoaController(List<Pessoa> listaPessoas) {
+            _listaPessoas = listaPessoas;
+        }
+
+
+        public IActionResult Index() {
+            return View(_listaPessoas);
+        }
+
+        public IActionResult Cadastrar() {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm] Pessoa pessoa) {
+            if (!ModelState.IsValid) {
+                return View(pessoa);
+            }
+            pessoa = GerarIdDaPessoa(pessoa);
+            _listaPessoas.Add(pessoa);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Editar(int id) {
+            var pessoa = _listaPessoas.FirstOrDefault(p => p.Id == id);
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public IActionResult Editar([FromForm] Pessoa pessoa) {
+            if (!ModelState.IsValid) {
+                return View(pessoa);
+            }
+
+            var pessoalista = _listaPessoas.FirstOrDefault(p => p.Id == pessoa.Id);
+
+            pessoalista.Nome = pessoa.Nome;
+            pessoalista.Telefone = pessoa.Telefone;
+            pessoalista.Email = pessoa.Email;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(int id) {
+            var pessoa = _listaPessoas.FirstOrDefault(p => p.Id == id);
+            _listaPessoas.Remove(pessoa);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private Pessoa GerarIdDaPessoa(Pessoa pessoa) {
+            var id = 0;
+
+            if (_listaPessoas.Count != 0) {
+                id = _listaPessoas.LastOrDefault().Id + 1;
+            }
+            pessoa.Id = id;
+
+            return pessoa;
+        }
+
+    }
+}
